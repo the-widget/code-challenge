e4eda85312471b939be4c0771cf4450849126442
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-21 14:31:55 -0400

    created PipeContacts class, prints comma and pipe contacts

--- lib/cyrus-code-challenge.rb
+++ lib/cyrus-code-challenge.rb
@@ -11,10 +11,17 @@ class CyrusCodeChallenge::CLI
       CommaContacts.new(contact[0], contact[1], contact[2], contact[3], contact[4])
     end
     
+    file = File.open("lib/input_files/pipe.txt")
+    contents = file.read
+    contacts_info = (contents.split("\n")).map{|x|x.split(" | ")}
+    contacts_info.each do |contact|
+      PipeContacts.new(contact[0], contact[1], contact[2], contact[3], contact[4], contact[5])
+    end
+    
     puts "Output 1:", ""
     Contacts.all.each do |contact|
       puts "#{contact.last_name} #{contact.first_name} #{contact.gender} #{contact.birth_date} #{contact.favorite_color}"
-    end
+    end    
   end
 end
 
@@ -33,6 +40,19 @@ class CommaContacts < Contacts
     @gender = gender
     @favorite_color = favorite_color
     @birth_date = birth_date
+    @@all << self
+  end
+end
+
+class PipeContacts < Contacts
+  attr_accessor :last_name, :first_name, :middle_initial, :gender, :favorite_color, :birth_date
+  def initialize(last_name, first_name, middle_initial, gender, favorite_color, birth_date)
+    @last_name = last_name
+    @first_name = first_name
+    @middle_initial = middle_initial
+    gender == "M" ? @gender = "Male" : @gender = "Female"
+    @favorite_color = favorite_color
+    @birth_date = birth_date.gsub(/[-]/, '/')
     @@all << self
   end
 end
