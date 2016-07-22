19e8850830baf4f02012cdca90699bd97aa64d7b
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-22 15:56:37 -0400

    space_parser_spec passing

---
+++ lib/space/space-parser.rb
@@ -0,0 +1,13 @@
+class SpaceParser
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def parse
+    file = File.open(@file_path)
+    contents = file.read
+    contents.split("\n").map{|x|x.split(" ")}
+  end
+end
+
+
