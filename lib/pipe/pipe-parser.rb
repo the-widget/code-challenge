5820d282bd591e59969bc59e02d8eeb45d0a44e3
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-22 09:29:47 -0400

    pipe-parser spec passing

---
+++ lib/pipe/pipe-parser.rb
@@ -0,0 +1,12 @@
+class PipeParser
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def parse
+    file = File.open(@file_path)
+    contents = file.read
+    contents.split("\n").map{|x|x.split(" | ")}
+  end
+end
