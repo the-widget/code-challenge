1bfed83c9e9c735ed2f3d7a4856fa5f488ad93b6
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-22 13:26:09 -0400

    created space-parser spec

--- lib/cyrus-code-challenge.rb
+++ lib/cyrus-code-challenge.rb
@@ -5,7 +5,7 @@ require_relative '../lib/comma/comma-contacts-creator'
 require_relative '../lib/pipe/pipe-parser'
 require_relative '../lib/pipe/pipe-contacts'
 require_relative '../lib/pipe/pipe-contacts-creator'
-
+require_relative '../lib/pipe/space-parser'
 
 module CyrusCodeChallenge
   VERSION = "0.0.0"
---
+++ spec/space/space-parser.rb
@@ -0,0 +1,24 @@
+require_relative "../../lib/cyrus-code-challenge"
+
+describe 'input_files' do
+  it 'inputs correctly formated data' do
+    file = File.open("lib/input_files/space.txt")
+    contents = file.read
+    expect(contents).to_not include(", ")
+    expect(contents).to_not include(" | ")
+  end
+end
+
+describe '.space parser' do
+  test_data = PipeParser.new("lib/input_files/space.txt").parse
+  
+  it 'parses into correct number of contacts' do
+    expect(test_data.size).to eq(3)
+  end
+
+  it 'parses the data correctly' do 
+    expect(test_data[0])
+    .to eq(["Kournikova", "Anna", "F", "F", "6-3-1975", "Red"])
+  end
+end
+
