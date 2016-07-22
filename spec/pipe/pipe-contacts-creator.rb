82773a2c2758ca9332ecda133b3bda2e5645890d
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-22 11:20:33 -0400

    created pipe-contacts-creator spec

---
+++ spec/pipe/pipe-contacts-creator.rb
@@ -0,0 +1,15 @@
+require_relative "../../lib/cyrus-code-challenge"
+
+describe '.pipe-contacts-creator' do
+  PipeContactsCreator.new.create
+
+  it 'creates contacts' do
+    expect(Contacts.all.length).to eq(3)
+    expect(Contacts.all[0].first_name).to eq("Steve")
+  end
+
+  it 'formats birth_date to Date class' do
+    expect(Contacts.all[0].birth_date).to be_an_instance_of(Date)
+  end
+
+end
