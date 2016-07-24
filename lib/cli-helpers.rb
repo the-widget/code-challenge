ee072b0efca0a0016d475b1328643fdb0e278db9
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-24 01:33:31 -0400

    refactored cli methods into helpers

--- lib/cli-helpers.rb
+++ lib/cli-helpers.rb
@@ -15,4 +15,31 @@ module CLIHelpers
   def red(text); colorize(text, 31); end
   def green(text); colorize(text, 32); end
 
+  def respond_to_input
+    input = gets.strip
+    case input
+    when "1"
+      clear;print "------------------------------------------\n"
+      @printer.output_1
+      print "------------------------------------------"
+    when "2"
+      clear;print "------------------------------------------\n"
+      @printer.output_2
+      print "------------------------------------------"
+    when "3"
+      clear;print "------------------------------------------\n"
+      @printer.output_3
+      print "------------------------------------------"
+    when "4"
+      clear;print "------------------------------------------\n"
+      @printer.print_all
+      print "------------------------------------------"
+    when "exit", "quit", "end", "q"
+      abort
+    else
+      puts red('Please choose a valid selection:')
+      respond_to_input
+    end
+  end
+
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -19,32 +19,11 @@ class CLI
     array.each{|x| print x;animate};sleep 0.6
     array = "\n\n1. Print Output 1\n2. Print Output 2\n3. Print Output 3\n4. Print Entire Solution\n".split("")
     array.each{|x| print x;animate}
-    respond
+    serve_user
   end
 
-  def respond
-    input = gets.strip
-    case input.to_i
-    when 1
-      clear;print "------------------------------------------\n"
-      @printer.output_1
-      print "------------------------------------------"
-    when 2
-      clear;print "------------------------------------------\n"
-      @printer.output_2
-      print "------------------------------------------"
-    when 3
-      clear;print "------------------------------------------\n"
-      @printer.output_3
-      print "------------------------------------------"
-    when 4
-      clear;print "------------------------------------------\n"
-      @printer.print_all
-      print "------------------------------------------"
-    else
-      puts red('Please choose a valid selection:')
-      respond
-    end
+  def serve_user
+    respond_to_input
     puts green("\nWould you like to make another selection? (y/n)")
     offer_another_selection
   end
