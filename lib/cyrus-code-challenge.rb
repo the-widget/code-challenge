a3ebe6448b81f79adc4e5c73dddb5b2cbc6bac0c
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-23 21:50:42 -0400

    updated require files

--- lib/cyrus-code-challenge.rb
+++ lib/cyrus-code-challenge.rb
@@ -4,11 +4,8 @@ require_relative './printer'
 require_relative '../lib/contacts'
 require_relative '../lib/formatter'
 require_relative '../lib/parser'
-require_relative '../lib/comma/comma-contacts'
 require_relative '../lib/comma/comma-contacts-creator'
-require_relative '../lib/pipe/pipe-contacts'
 require_relative '../lib/pipe/pipe-contacts-creator'
-require_relative '../lib/space/space-contacts'
 require_relative '../lib/space/space-contacts-creator'
 
 module CyrusCodeChallenge
