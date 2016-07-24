0cc1a711969d67587ca9d5e6a08d8b5fce69f1d0
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-24 17:00:49 -0400

    fixed input files load path

--- lib/cyrus-code-challenge/contacts-creator.rb
+++ lib/cyrus-code-challenge/contacts-creator.rb
@@ -1,11 +1,12 @@
 class ContactsCreator
+  
   def self.call
-    Dir.glob("lib/input_files/*.txt") do |txt_file|
+    input_files = File.expand_path("../../data/", File.dirname(__FILE__))
+    Dir.glob("#{input_files}/*.txt") do |txt_file|
       contacts_info = Parser.new(txt_file).parse
       contacts_info.each do |contact|
         Contacts.new(contact[0], contact[1], contact[2], contact[3], contact[4])
       end
     end
   end
-
 end
