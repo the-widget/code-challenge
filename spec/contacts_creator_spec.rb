1e79b6eb45bfec1c91d4720f2a091788e60ce1f8
brennenawana108ny@gmail.com <brennenawana108ny@gmail.com>
2016-07-23 22:16:26 -0400

    created contacts_creator_spec, to refactor all creators into a single class

---
+++ spec/contacts_creator_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../../lib/cyrus-code-challenge"
+
+describe '.contacts-creator' do
+  ContactsCreator.new.create
+
+  it 'creates all contacts' do
+    expect(Contacts.all.length).to eq(9)
+    expect(Contacts.all[0].first_name).to eq("Neil")
+    expect(Contacts.all.last.birth_date).to eq("12/2/1973")
+  end
+
+  it ''
+
+  it 'formats birth_date to Date class' do
+    expect(Contacts.all[0].birth_date).to be_an_instance_of(Date)
+  end
+end
